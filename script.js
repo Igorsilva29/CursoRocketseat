322aa7ecf7cafaa49e5a5f1e5ce22ec9797d29e4
Igorsilva29 <igorjuniocontato@gmail.com>
2025-07-04 13:18:25 -0300

    Alterar imagem no modo light

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 function toggleMode() {
     const html = document.documentElement
+    html.classList.toggle('light')
 
-/* Uma das formas de fazer a função de mudar o modo
-
-   if(html.classList.contains('ligth')) {
-        html.classList.remove('light')
-    } else {
-        html.classList.add('light')
-    } */
-   
-   html.classList.toggle('light')
+    // Pegar a tag img 
+    const img=document.querySelector("#profile img")
+    // substituir a imagem
+   if(html.classList.contains('light')) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute('src', './img/avatar-light.png')
+   } else {
+    // se tiver sem light mode, manter a imagem normal
+    img.setAttribute('src','./img/avatar.png')
+   }
 }
